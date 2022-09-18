ee529ceceb9045f837a7c3bb3dbaa37ebb14ff4f
chane81 <chane81@naver.com>
2022-09-19 01:26:38 +0900

    refactor: jest setup

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,19 @@
 const config = {
   roots: ['<rootDir>/src/__tests__/'],
   testEnvironment: 'jsdom',
+  // style mock
   moduleNameMapper: {
     '\\.(css|less|svg|scss)$': 'identity-obj-proxy'
   },
   testRegex: '.spec.ts(x)$',
-  //setupFiles: ['@testing-library/react/dont-cleanup-after-each'],
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
+  // 자동 cleanup 방지 - code 에서 cleanup 하기위함
+  setupFiles: ['@testing-library/react/dont-cleanup-after-each'],
+  // setupTest, extends jest
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts', '@testing-library/jest-dom/extend-expect'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
+  // transform ignore
   transformIgnorePatterns: [
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
     '\\.(css|less|svg|scss)$'
@@ -20,6 +24,7 @@ const config = {
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
   ],
+  collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts'
